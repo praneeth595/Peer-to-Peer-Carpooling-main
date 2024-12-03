17d549ca3f8eb3609f2d48725f64f82010221746
praneeth595 <83818698+praneeth595@users.noreply.github.com>
2024-12-03 12:19:43 +0530

    Delete Peer-to-Peer-Carpooling-main directory

--- Peer-to-Peer-Carpooling-main/carpool/website/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from website.models import Customer, Booking, ContactUs, Mycar
-
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(Mycar)
-admin.site.register(ContactUs)
-admin.site.register(Booking)
